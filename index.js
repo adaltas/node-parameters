d03f474a4f589f79239e9203fb424415500c4673
David Worms <david@adaltas.com>
2011-08-09 00:01:34 +0200

    Move Shell at the root level

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 require('coffee-script')
 
-module.exports = {
-    // Core
-    Shell: require('./lib/Shell'),
-    styles: require('./lib/Styles'),
-    // Plugins
-    cloud9: require('./lib/plugins/cloud9'),
-    coffee: require('./lib/plugins/coffee'),
-    completer: require('./lib/plugins/completer'),
-    error: require('./lib/plugins/error'),
-    help: require('./lib/plugins/help'),
-    history: require('./lib/plugins/history'),
-    http: require('./lib/plugins/http'),
-    router: require('./lib/plugins/router'),
-    redis: require('./lib/plugins/redis'),
-    test: require('./lib/plugins/test'),
-    routes: {
-        shellOnly: require('./lib/routes/shellOnly')
-    }
+// Core
+var Shell = module.exports = require('./lib/Shell');
+Shell.styles = require('./lib/Styles');
+
+// Plugins
+Shell.cloud9 = require('./lib/plugins/cloud9');
+Shell.coffee = require('./lib/plugins/coffee');
+Shell.completer = require('./lib/plugins/completer');
+Shell.error = require('./lib/plugins/error');
+Shell.help = require('./lib/plugins/help');
+Shell.history = require('./lib/plugins/history');
+Shell.http = require('./lib/plugins/http');
+Shell.router = require('./lib/plugins/router');
+Shell.redis = require('./lib/plugins/redis');
+Shell.test = require('./lib/plugins/test');
+
+// Routes
+Shell.routes = {
+    shellOnly: require('./lib/routes/shellOnly')
 };
 
+Shell.Shell = function(settings){
+    console.warn('Deprecated, use `shell()` instead of `shell.Shell()`');
+    return new Shell( settings );
+}
